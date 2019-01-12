2999ec87ab2ee0d8ebf7d59090b1fe1bba4658b0
Noa <noadiss@gmail.com>
2019-01-13 01:31:19 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,8 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {CommentsService} from '../../services/comments/comments.service';
-import {Comment} from '../../models/comment';
-import {fromEvent} from 'rxjs';
-import {debounceTime, map} from 'rxjs/operators';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<app-search-comments></app-search-comments>'
 })
-export class AppComponent implements OnInit {
-  @ViewChild('searchBox') searchBox: ElementRef;
-  private comments: Comment[];
-  private filteredComments: Comment[];
-
-  constructor(private commentsService: CommentsService) {
-  }
-
-  ngOnInit(): void {
-    this.subscribeSearch();
-  }
-
-  public getComments(postId: string): void {
-    this.commentsService.getCommentsByPostId(postId).subscribe(comments => {
-      console.log(comments);
-      this.comments = comments;
-      this.filteredComments = comments;
-    });
-  }
-
-  private subscribeSearch(): void {
-    const inputEvent = fromEvent(this.searchBox.nativeElement, 'input').pipe(map((evt: any) => evt.target.value));
-    const debouncedInput = inputEvent.pipe(debounceTime(500));
-    const subscribe = debouncedInput.subscribe(val => this.filterComments(val));
-  }
-
-  private filterComments(filter: string): void {
-    this.filteredComments = this.comments.filter((comment: Comment) => comment.body.includes(filter));
-  }
+export class AppComponent {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CommentsService} from './services/comments/comments.service';
+import {HttpClientModule} from '@angular/common/http';
+import {CommentsListComponent} from './components/comments-list/comments-list.component';
+import {CommentComponent} from './components/comment/comment.component';
+import {ConfigService} from './services/config/config.service';
+import { SearchCommentsComponent } from './components/search-comments/search-comments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentsListComponent,
+    CommentComponent,
+    SearchCommentsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CommentsService, ConfigService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
